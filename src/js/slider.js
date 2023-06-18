8ccd009da811fa678b0daac3c16c877cb98b26c9
Vladislav <93011297+VladislavApelhants@users.noreply.github.com>
2023-06-18 16:53:20 +0300

    Slider

---
+++ src/js/slider.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper('.mySwiper', {
+        effect: 'cube',
+        loop: true,
+        grabCursor: true,
+        cubeEffect: {
+          shadow: true,
+          slideShadows: true,
+          shadowOffset: 20,
+          shadowScale: 0.94,
+        },
+        pagination: {
+          el: '.swiper-pagination',
+        },
+      });
